ec0adde5ff3d10f0d0b3ec9a051485c33ef8cae2
codeme254 <otwomadennis365@gmail.com>
2022-09-20 11:08:21 +0300

    coded the sign up form and made the auth page responsive

--- client/mentorship-cradle/src/AuthPage/AuthPage.jsx
+++ client/mentorship-cradle/src/AuthPage/AuthPage.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import careerImg from "../Assets/Images/career.svg";
 import { User, Key } from "../Icons/Icons";
 // sign-in-up-form
 const AuthPage = () => {
+  const formsRef = useRef(null);
+  // const buttonsRef = useRef(null);
+  const handleToggle = (e) => {
+    const formToShow = e.target.dataset.sib;
+    console.log(formToShow);
+
+    const forms = Array.from(formsRef.current.children);
+
+    forms.forEach((form) => {
+      if (form.dataset.sib === formToShow) {
+        form.classList.add("form--active");
+        form.classList.remove("form--sleeping");
+      } else {
+        form.classList.remove("form--active");
+        form.classList.add("form--sleeping");
+      }
+    });
+  };
   return (
     <div className="authentication-page">
       <div className="auth-page-container">
@@ -16,38 +34,135 @@ const AuthPage = () => {
         <div className="forms__container">
           <div className="forms__container--header">
             <div className="auth__toggle-buttons">
-              <button className="auth-toggle__button">login</button>
-              <button className="auth-toggle__button">sign up</button>
+              <button
+                className="auth-toggle__button"
+                data-sib="login"
+                onClick={handleToggle}
+              >
+                login
+              </button>
+              <button
+                className="auth-toggle__button"
+                data-sib="signup"
+                onClick={handleToggle}
+              >
+                sign up
+              </button>
             </div>
             <h1 className="auth--title">MentorshipCradle</h1>
           </div>
 
-          <div className="forms">
-            <form action="get" className="login-form">
-                <h2 className="auth-form-title">Log into your account.</h2>
-                {/* username */}
+          <div className="forms" ref={formsRef}>
+            <form action="get" className="login-form" data-sib="login">
+              <h2 className="auth-form-title">Log into your account.</h2>
+              {/* username */}
               <div className="form-input-wrapper">
-                <label for="name" className="form-input-icon">
-                    <User />
+                <label htmlFor="name" className="form-input-icon">
+                  <User />
                 </label>
-                <input type="text" name="" id="name" className="auth-form-text-input" placeholder="Username eg Jack Dossey" />
+                <input
+                  type="text"
+                  name=""
+                  id="name"
+                  className="auth-form-text-input"
+                  placeholder="Username eg Jack Dossey"
+                />
               </div>
               {/* password */}
               <div className="form-input-wrapper">
-                <label for="password" className="form-input-icon">
-                    <Key />
+                <label htmlFor="password" className="form-input-icon">
+                  <Key />
                 </label>
-                <input type="password" name="" id="password" className="auth-form-text-input" placeholder="Password" />
+                <input
+                  type="password"
+                  name=""
+                  id="password"
+                  className="auth-form-text-input"
+                  placeholder="Password"
+                />
               </div>
               {/* submit button */}
-              <a href="/" className="auth-submit-btn">sign in</a>
-              <a href="/" className="forgot-password">forgot your password ?</a>
+              <a href="/" className="auth-submit-btn">
+                sign in
+              </a>
+              <a href="/" className="forgot-password">
+                forgot your password ?
+              </a>
             </form>
-            <form action="post" className="signup-form">
-              {/* <input type="text" name="name" id="" className="form--input" />
-              <input type="text" name="name" id="" className="form--input" />
-              <input type="text" name="name" id="" className="form--input" />
-              <input type="text" name="name" id="" className="form--input" /> */}
+
+            {/* sign up form */}
+            <form action="post" className="signup-form" data-sib="signup">
+              <div className="form__flex-box">
+                <div className="form-col-input-wrapper">
+                  <label htmlFor="firstname" className="form-col-lbl">
+                    first name
+                  </label>
+                  <input
+                    type="text"
+                    id="firstname"
+                    className="form__col--input"
+                    placeholder="first name"
+                  />
+                </div>
+                <div className="form-col-input-wrapper">
+                  <label htmlFor="lastname" className="form-col-lbl">
+                    last name
+                  </label>
+                  <input
+                    type="text"
+                    id="lastname"
+                    className="form__col--input"
+                    placeholder="last name"
+                  />
+                </div>
+              </div>
+              <div className="form-col-input-wrapper">
+                <label htmlFor="email" className="form-col-lbl">
+                  email address
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className="form__col--input"
+                  placeholder="last name"
+                />
+              </div>
+              <div className="form-col-input-wrapper">
+                <label htmlFor="pass" className="form-col-lbl">
+                  password
+                </label>
+                <input
+                  type="password"
+                  id="pass"
+                  className="form__col--input"
+                  placeholder="create your password"
+                />
+              </div>
+              <div className="form-col-input-wrapper">
+                <label htmlFor="conf-pass" className="form-col-lbl">
+                  confirm password
+                </label>
+                <input
+                  type="password"
+                  id="conf-pass"
+                  className="form__col--input"
+                  placeholder="confirm password"
+                />
+              </div>
+              <div className="form-col-input-wrapper">
+                <label htmlFor="org" className="form-col-lbl">
+                  Your organisation
+                </label>
+                <input
+                  type="text"
+                  id="org"
+                  className="form__col--input"
+                  placeholder="name of your organisation"
+                />
+              </div>
+              <a href="/" className="sign-up-btn">
+                sign me up
+              </a>
             </form>
           </div>
         </div>
