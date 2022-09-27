43a52608eef6cc25979eecff60a14bc02604cef8
codeme254 <otwomadennis365@gmail.com>
2022-09-27 13:47:55 +0300

    code transformation

--- client/mentorship-cradle/src/Pages/MentorSpotlight/FlipCard.jsx
+++ client/mentorship-cradle/src/Pages/MentorSpotlight/FlipCard.jsx
@@ -1,8 +1,43 @@
 import React from "react";
+import img from "../../Assets/Images/img6.jpg";
 
-const FlipCard = () => {}
+const FlipCard = ({ image, title, role }) => {
+  return (
+    <div className="flip">
+      <div className="wrapper">
+        <div className="card front-face">
+          <img src={img} alt="_image" />
+        </div>
+        <div className="card back-face">
+          <img src={image} alt="_image" />
+          <div className="info">
+            <div className="title">{title}</div>
+            <p>{role}</p>
+          </div>
+          <ul>
+            <a href="#">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="fab fa-youtube"></i>
+            </a>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-{/* <div class="flip">
+export default FlipCard;
+
+{
+  /* <div class="flip">
 <div class="wrapper">
     <div class="card front-face">
       <img src="images/img6.jpg" alt="">
@@ -21,4 +56,5 @@ const FlipCard = () => {}
       </ul>
     </div>
   </div>
-</div> */}
+</div> */
+}
--- client/mentorship-cradle/src/Pages/MentorSpotlight/MentorSpotlight.jsx
+++ client/mentorship-cradle/src/Pages/MentorSpotlight/MentorSpotlight.jsx
@@ -1,32 +1,40 @@
 import React from "react";
 import Spotlight from "./Spotlight";
 import sampImg from "../../Assets/Images/img1.jpg";
+import sampImg2 from "../../Assets/Images/img5.jpg";
 import Header from "../../Components/Header";
+import FlipCard from "./FlipCard";
 
 const MentorSpotlight = () => {
   return (
     <div>
-        <Header />
-    <div className="mentor">
-      <h1>mentor spotlight</h1>
+      <Header />
+      <div className="mentor">
+        <h1>mentor spotlight</h1>
 
-      <div className="container">
-        <div class="indicator">
-          <span class="btn active"></span>
-          <span class="btn"></span>
-          <span class="btn"></span>
-          <span class="btn"></span>
+        <div className="container">
+          <div class="indicator">
+            <span class="btn active"></span>
+            <span class="btn"></span>
+            <span class="btn"></span>
+            <span class="btn"></span>
+          </div>
+
+          <Spotlight
+            name="chike agnes"
+            location="zumi africa"
+            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+            image={sampImg}
+          />
+          {/* I should not go below this line for now */}
         </div>
-
-        <Spotlight
-          name="chike agnes"
-          location="zumi africa"
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-          image={sampImg}
-        />
-        {/* I should not go below this line for now */}
       </div>
-    </div>
+      {/* the 3d flip cards */}
+      <FlipCard
+            image={sampImg2}
+            title="my title"
+            role="senior frontend engineer"
+          />
     </div>
   );
 };
