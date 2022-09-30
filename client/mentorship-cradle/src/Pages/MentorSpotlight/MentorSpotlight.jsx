9314d6a0bbca5042bb2e48e05c8d9a9a310bd46b
codeme254 <otwomadennis365@gmail.com>
2022-09-30 15:31:10 +0300

    final fixing of the mentor spotlight page

--- client/mentorship-cradle/src/Pages/MentorSpotlight/MentorSpotlight.jsx
+++ client/mentorship-cradle/src/Pages/MentorSpotlight/MentorSpotlight.jsx
@@ -1,6 +1,11 @@
-import React, { useRef } from "react";
+import React from "react";
+import Carousel from "react-elastic-carousel";
+
 import Spotlight from "./Spotlight";
 import sampImg from "../../Assets/Images/img1.jpg";
+import sampImg3 from "../../Assets/Images/img3.jpg";
+import sampImg4 from "../../Assets/Images/img4.jpg";
+import sampImg5 from "../../Assets/Images/img2.jpg";
 import sampImg2 from "../../Assets/Images/img5.jpg";
 import Header from "../../Components/Header";
 import FlipCard from "./FlipCard";
@@ -13,14 +18,7 @@ const MentorSpotlight = () => {
         <h1>mentor spotlight</h1>
 
         <div className="container">
-          <div class="indicator" ref={allButtonsContainerRef}>
-            <span class="btn active"></span>
-            <span class="btn"></span>
-            <span class="btn"></span>
-            <span class="btn"></span>
-          </div>
-
-          <div className="sliding-row" id="slide">
+          <Carousel>
             <Spotlight
               name="chike agnes"
               location="zumi africa"
@@ -32,9 +30,22 @@ const MentorSpotlight = () => {
               name="chike agnes"
               location="zumi africa"
               text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-              image={sampImg}
+              image={sampImg3}
             />
-          </div>
+            <Spotlight
+              name="chike agnes"
+              location="zumi africa"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              image={sampImg4}
+            />
+
+            <Spotlight
+              name="chike agnes"
+              location="zumi africa"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              image={sampImg5}
+            />
+          </Carousel>
         </div>
       </div>
       {/* the 3d flip cards */}
@@ -45,22 +56,35 @@ const MentorSpotlight = () => {
       />
 
       <div className="mentor">
-        <h1>mentor spotlight</h1>
-
         <div className="container">
-          <div class="indicator">
-            <span class="btn active"></span>
-            <span class="btn"></span>
-            <span class="btn"></span>
-            <span class="btn"></span>
-          </div>
+          <Carousel>
+            <Spotlight
+              name="chike agnes"
+              location="zumi africa"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              image={sampImg}
+            />
 
-          <Spotlight
-            name="chike agnes"
-            location="zumi africa"
-            text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-            image={sampImg}
-          />
+            <Spotlight
+              name="chike agnes"
+              location="zumi africa"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              image={sampImg3}
+            />
+            <Spotlight
+              name="chike agnes"
+              location="zumi africa"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              image={sampImg4}
+            />
+
+            <Spotlight
+              name="chike agnes"
+              location="zumi africa"
+              text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+              image={sampImg5}
+            />
+          </Carousel>
         </div>
       </div>
 
